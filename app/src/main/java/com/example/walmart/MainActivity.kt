473679af793049954261d867f5b1803b9b50458a
Ahmed Fayez <a.fayez707@gmail.com>
2022-12-12 21:47:45 -0600

    Assignment_3

---
+++ app/src/main/java/com/example/walmart/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.walmart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+ * By: Ahmed Fayez
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/test/java/com/example/walmart/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.example.walmart
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ * By: Ahmed Fayez
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(8, 4 + 4)
+    }
+}
